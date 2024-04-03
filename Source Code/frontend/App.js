f1f93f66f76a91b5228bf565f7e166a68c4a95b3
MashoodCk <mashoodck77@gmail.com>
2024-04-04 00:14:39 +0530

    frontend added, fix issue

---
+++ Source Code/frontend/App.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './pages/loginScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <LoginScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Source Code/frontend/pages/loginScreen.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+    
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <Text style={styles.subTitle}>Smart Attendence System</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'aliceblue',
+    width: '100%'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subTitle:{
+    fontSize: 16,
+    marginBottom: 20
+  },
+  input: {
+    width: '80%',
+    height: 50,
+    backgroundColor: '#ffffff',
+    marginBottom: 10,
+    paddingLeft: 15,
+    borderRadius: 5,
+  },
+  button: {
+    width: '80%',
+    height: 50,
+    backgroundColor: '#007bff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default LoginScreen;
