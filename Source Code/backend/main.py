161d50144b6fe09026b2be738a9c637e4ca0be9e
BassamEC <baschola@gmail.com>
2024-04-23 00:28:57 +0530

    backend image linking

--- Source Code/backend/main.py
+++ Source Code/backend/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 from database import dbconnector
-
-
+from werkzeug.utils import secure_filename
+import os
+from model import predict_faces
 app = Flask(__name__)
 
 
@@ -35,5 +36,32 @@ def student_login():
     return jsonify({'success': success})
 
 
+UPLOAD_FOLDER = r'Source Code\backend\img'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '' in filename and filename. rsplit('.', 1) [1]. lower() in ALLOWED_EXTENSIONS
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+@app. route('/media/upload', methods= ['POST' ])
+def upload_media():
+    if 'file' not in request.files:
+        return jsonify({'error': 'media not provided'}), 400
+    file = request. files ['file']
+    if file.filename =='':
+        return jsonify({'error': 'no file selected'}), 400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file. filename)
+    file.save(os.path.join(app.config ['UPLOAD_FOLDER'], filename) )
+    prediction=predict_faces(file)
+    print(prediction)
+    return jsonify({'msg': 'media uploaded successfully'})
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, host="192.168.1.37") #server ip address
+    #app.run(debug=True) 
